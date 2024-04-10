fd27c2c5071d1733a613e71da1e388021a3d9f93
djinjones <160979104+djinjones@users.noreply.github.com>
2024-04-09 21:34:29 -0500

    updated fetch funciton

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,41 @@ const searchBtn = document.querySelector('#searchBtn');
 const searchBar = document.querySelector('#searchBar');
 const selectField = document.querySelector('#selectField');
 
+
+
 function getUserLocation() {
     //We need to get the user's current location so we know how far away breweries are from the user 
 }
 
-function handleFormSubmit() {
+function handleFormSubmit(event) {
+    event.preventDefault();
     //We need to get the data from the form and send it through the fetch function
-    fetchBreweryData(city, parameter);
-    
+    let parameter = selectField.value;
+    const searchValue = searchBar.value;
+    parameter = `${parameter}=${searchValue}`
+    fetchBreweryData(parameter);
 }
-function fetchBreweryData(city, parameter) {
+
+function fetchBreweryData(parameter) {
     //We need to take the data inputed from the form and use it to search for breweies through the API
+    const baseAPIurl = 'https://api.openbrewerydb.org/v1/breweries';
+    const fetchUrl = `${baseAPIurl}?${parameter}&per_page=15`
+    
+    async function fetchOpenBreweryDB() {
+        try {
+            const response = await fetch(fetchUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            localStorage.setItem('brewery-data', JSON.stringify(data)); 
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching brewery data:', error);
+        }
+    }
+
+    fetchOpenBreweryDB();
 }
 
 function appendBreweryData() {
@@ -39,6 +63,28 @@ function addBreweryToDonelist() {
 
 
 
-searchBtn.addEventListener('click', function(){
-    handleFormSubmit();
+searchBtn.addEventListener('click', function(event){
+    handleFormSubmit(event);
+})
+
+selectField.addEventListener('change', function(){
+    const selectedOption = selectField.value;
+        switch (selectedOption) {
+            case 'by_name':
+              searchBar.placeholder = 'Search by Name';
+              break;
+            case 'by_dist':
+              searchBar.placeholder = 'Search by Distance';
+              break;
+            case 'by_city':
+              searchBar.placeholder = 'Search by City';
+              break;
+            case 'by_state':
+              searchBar.placeholder = 'Search by State';
+              break;
+
+            default:
+              searchBar.placeholder = 'Search by Name';
+              break;
+    }
 })
